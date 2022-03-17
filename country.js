9fa9c95fe9aae45300c3733f45457ff02a10f6d2
afsanaghoni <piyal.afsana@gmail.com>
2022-03-17 19:02:46 +0600

    set api

---
+++ country.js
@@ -0,0 +1,12 @@
+console.log('hi');
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    console.log(countries);
+
+}
+loadCountries();
